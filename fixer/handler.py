22f2524dc8e084805ddc668cc35b44ec7833ab68
web-test <alypwr573@gmail.com>
2024-08-25 06:05:46 -0700

    init

---
+++ fixer/handler.py
@@ -0,0 +1,2 @@
+def get_rates(api_key):
+    print("gfjg")
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="fixer",
+      version="1",
+      author="Abolfazl Alipour",
+      author_email="alypwr573@gmail.com",
+      url="https://github.com/Abolfazl13832/fixer.git",
+      packages=['fixer'])
